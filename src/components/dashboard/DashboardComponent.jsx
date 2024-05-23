feea23529916cad9d43e3bd20e8957e297671ded
ArySuarsyah <gotrany97@gmail.com>
2024-05-23 17:39:42 +0700

    feat: dashboar

---
+++ src/components/dashboard/DashboardComponent.jsx
@@ -0,0 +1,14 @@
+
+import DashboardLayout from '../shared/DashboardLayout'
+
+const DashboardComponent = () => {
+  return (
+    <>
+      <DashboardLayout>
+        <h1>Overview</h1>
+      </DashboardLayout>
+    </>
+  )
+}
+
+export default DashboardComponent
---
+++ src/containers/dashboard/DashboardContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashboardComponent from '../../components/dashboard/DashboardComponent'
+
+const DashboardContainer = () => {
+  return (
+    <>
+      <DashboardComponent/>
+    </>
+  )
+}
+
+export default DashboardContainer
