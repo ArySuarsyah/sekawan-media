fa837c700e4b4ee81a422285d2d37ccac1c7033c
ArySuarsyah <gotrany97@gmail.com>
2024-05-24 02:28:12 +0700

    refactor: ticket component

--- src/components/shared/AllTickets.jsx
+++ src/components/shared/AllTickets.jsx
@@ -5,19 +5,50 @@ import fetchDataFromBackend from "../../../utils/api";
 import moment from "moment";
 import { FaArrowUpWideShort } from "react-icons/fa6";
 import { FaFilter } from "react-icons/fa";
+import { filterData, paginateData, sortData } from "../../../utils/filter";
 
 const AllTicket = () => {
   const [data, setData] = useState(null);
+  const [startIndex, setStartIndex] = useState(1);
+  const [filterKey, setFilterKey] = useState('')
+  const [showFilter, setShowFilter] = useState(false)
+  const [sortBy, setSortBy] = useState("name");
 
   useEffect(() => {
     fetchDataFromBackend()
       .then((data) => {
-        setData(data);
+        const filteredData = filterKey ? filterData(data, filterKey) : data;
+        const sortedData = sortData(filteredData, sortBy, "asc");
+        setData(paginateData(sortedData, startIndex, 10));
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       });
-  }, []);
+  }, [startIndex, filterKey, sortBy]);
+
+  const handleSort = (column) => {
+    setSortBy(column)
+  };
+
+
+  // Pagination
+  const paginationNext = () => {
+    if (data.length - 1 === startIndex) {
+      return;
+    } else {
+      setStartIndex(startIndex + 1);
+    }
+  };
+
+  const paginationPrev = () => {
+    if (startIndex > 1) {
+      setStartIndex(startIndex - 1);
+    } else {
+      setStartIndex(1);
+    }
+  };
+
+
 
   console.log(data)
   return (
@@ -25,15 +56,36 @@ const AllTicket = () => {
       <div className="grid grid-cols-2 mb-2">
         <h2 className="text-2xl font-semibold">Title</h2>
         <div className="flex justify-end gap-5">
-          <div className="flex items-center gap-2 cursor-pointer">
+          <div
+            className="flex items-center gap-2 cursor-pointer">
             <FaArrowUpWideShort className="text-gray-500" />
-            <span className="text-xs text-slate-300">
-              Short
-            </span>
+
+            <div className="dropdown dropdown-end">
+              <div tabIndex={0} role="button" className="text-xs text-slate-300 m-1">Short</div>
+              <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-white text-base-100 rounded-box w-52">
+                <li
+                  onClick={() => handleSort("name", "asc")}
+                >
+                  <a>a-z</a>
+                </li>
+                <li
+                  onClick={() => handleSort("-name", "desc")}
+                >
+                  <a>z-a</a>
+                </li>
+              </ul>
+            </div>
+
           </div>
-          <div className="flex items-center gap-2 cursor-pointer">
+          {showFilter && (
+            <input type="text" placeholder="Search" className="input h-8 bg-white" onChange={(e) => setFilterKey(e.target.value)} />
+          )}
+          <div
+            onClick={() => setShowFilter(!showFilter)}
+            className="flex items-center gap-2 cursor-pointer">
             <FaFilter className="text-gray-500" />
-            <span className="text-xs text-slate-300">
+            <span
+              className="text-xs text-slate-300">
               Filter
             </span>
           </div>
@@ -51,7 +103,6 @@ const AllTicket = () => {
             </tr>
           </thead>
           <tbody>
-            {/* row 1 */}
             {
               data?.map((value, index) =>
                 <tr
@@ -91,7 +142,7 @@ const AllTicket = () => {
                       <span className="text-xs bg-yellow-300 px-5 py-2 rounded-full w-96">LOW</span>
                     }
                     {
-                      value.priority > 3 && value.priority < 6 ?
+                      value.priority > 3 && value.priority <= 6 ?
                         (<span className="text-xs bg-cyan-500 px-5 py-2 rounded-full w-96">NORMAL</span>) : ''
                     }
                     {
@@ -106,6 +157,13 @@ const AllTicket = () => {
             }
           </tbody>
         </table>
+        <div className="join">
+          <button onClick={paginationPrev} className="join-item btn">«</button>
+          <button className="join-item btn">
+            {startIndex}
+          </button>
+          <button onClick={paginationNext} className="join-item btn">»</button>
+        </div>
       </div>
     </div>
   );
