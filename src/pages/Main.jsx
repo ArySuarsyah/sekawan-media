99f96d4a16a6bc63ba1146fa604d0ec8ae3f1fcf
ArySuarsyah <gotrany97@gmail.com>
2024-05-23 17:37:11 +0700

    feat: add dashboard route

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginContainer from "../containers/auth/LoginContainer";
+import DashboardContainer from "../containers/dashboard/DashboardContainer";
 
 function Main() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<DashboardContainer />} />
         <Route path="/login" element={<LoginContainer />} />
       </Routes>
     </BrowserRouter>
